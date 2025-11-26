e3934d4c215d98783594a8c4fe6bd2b10a6b858b
LiuYuhui <153437088+Lorena-Y-Liu@users.noreply.github.com>
2025-11-26 17:58:39 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/liuyuhui.github.io/', // Important: keeps paths relative to root for username.github.io
+  base: '/', // Important: keeps paths relative to root for username.github.io
 })
